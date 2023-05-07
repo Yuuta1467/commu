5355ece93efd9f3968ed696e25a386e137258947
Yuuta1467 <s1341011we@gmail.com>
2023-05-07 07:23:05 +0000

    [マイページ途中]

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,10 +1,39 @@
 class Public::UsersController < ApplicationController
+  before_action :authenticate_user!
+
   def show
+    @user = current_user
   end
 
   def edit
+    @user = current_user
+  end
+
+  def update
+    @user = current_user
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: "変更が成功しました。"
+    else
+      render :edit
+    end
   end
 
   def check
+    @user = current_user
+  end
+
+  def withdraw
+    @user = current_user
+    # is_deletedカラムをtrueに変更することにより削除フラグを立てる
+    @user.update(is_deleted: true)
+    reset_session
+    flash[:notice] = "退会処理を実行いたしました"
+    redirect_to root_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :type, :email)
   end
 end
