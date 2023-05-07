c17ef499e82b0ed98a9c30c7c6879489ade83cf7
Yuuta1467 <s1341011we@gmail.com>
2023-05-07 08:57:41 +0000

    [マイページユーザー情報部分作成]

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,4 +15,9 @@ class User < ApplicationRecord
          def active_for_authentication?
           super && (self.is_deleted == false)
          end
+
+         def get_profile_image
+           (profile_image.attached?) ? profile_image : 'no_image.jpg'
+         end
+
 end
