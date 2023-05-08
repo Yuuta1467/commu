a5f72bdfd553ea6590b2b901eced7f32f78d1459
Yuuta1467 <s1341011we@gmail.com>
2023-05-08 09:20:45 +0000

    [ゲストログイン修正]

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,6 +1,6 @@
 class Public::PostsController < ApplicationController
-  before_action :authenticate_user!
-  before_action :ensure_correct_user, only: [:edit, :update, :destroy]
+  #before_action :authenticate_user!
+  #before_action :ensure_correct_user, only: [:edit, :update, :destroy]
 
   def new
     @post = Post.new
@@ -40,19 +40,6 @@ class Public::PostsController < ApplicationController
     redirect_to posts_path
   end
 
-  private
-
-  def post_params
-    params.require(:post).permit(:content, :tag)
-  end
-
-  def ensure_correct_user
-     @post = Post.find(params[:id])
-    unless @post.user == current_user
-     redirect_to posts_path
-    end
-  end
-
   def guest_sign_in
     user = User.find_or_create_by!(email: 'guest@example.com') do |user|
       user.password = SecureRandom.urlsafe_base64
@@ -63,4 +50,18 @@ class Public::PostsController < ApplicationController
     sign_in user
     redirect_to posts_path#, notice: 'ゲストユーザーとしてログインしました。'
   end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content, :tag)
+  end
+
+  #def ensure_correct_user
+     #@post = Post.find(params[:id])
+    #unless @post.user == current_user
+     #redirect_to posts_path
+    #end
+  #end
+
 end
