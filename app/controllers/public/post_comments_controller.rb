6cdb2e6c996e691f079a69ea6b8f1ccb3beedb8e
Yuuta1467 <s1341011we@gmail.com>
2023-05-09 11:26:28 +0000

    [コメント投稿、編集、削除機能作成]

--- app/controllers/public/post_comments_controller.rb
+++ app/controllers/public/post_comments_controller.rb
@@ -1,4 +1,51 @@
 class Public::PostCommentsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :ensure_correct_user, only: [:edit, :update, :destroy]
+
   def edit
+    @post = Post.find(params[:post_id])
+    @post_comment = PostComment.find(params[:id])
+
   end
+
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = current_user.post_comments.new(post_comment_params)
+    @comment.post_id = @post.id
+    @comment.save
+    redirect_to post_path(@post)
+  end
+
+  def update
+    @post = Post.find(params[:post_id])
+    @post_comment = PostComment.find(params[:id])
+    if @post_comment.update(post_comment_params)
+      #flash[:success] = "Comment updated"
+      redirect_to @post
+    else
+      #flash[:danger] = "Comment failed"
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:post_id])
+    @post_comment = @post.post_comments.find(params[:id])
+    @post_comment.destroy
+    redirect_to post_path(params[:post_id])
+  end
+
+  private
+
+  def post_comment_params
+    params.require(:post_comment).permit(:comment, :post_id)
+  end
+
+  def ensure_correct_user
+     @post_comment = PostComment.find(params[:id])
+    unless @post_comment.user == current_user
+     redirect_to posts_path
+    end
+  end
+
 end
--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -14,6 +14,7 @@ class Public::PostsController < ApplicationController
 
   def index
     @posts = Post.all
+    @post_comments = PostComment.all
   end
 
   def create
