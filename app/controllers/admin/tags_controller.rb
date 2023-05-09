7bdd054258be0f81f787006bb899813a5e3fe86c
Yuuta1467 <s1341011we@gmail.com>
2023-05-09 05:50:48 +0000

    [タグ登録、一覧、編集機能作成]

--- app/controllers/admin/tags_controller.rb
+++ app/controllers/admin/tags_controller.rb
@@ -1,7 +1,33 @@
 class Admin::TagsController < ApplicationController
+  #before_action :authenticate_admin!
   def index
+    @tag = Tag.new
+    @tags = Tag.all
+  end
+
+  def create
+    @tag = Tag.new(tag_params)
+    @tag.save
+    redirect_to admin_tags_path
   end
 
   def edit
+    @tag = Tag.find(params[:id])
+  end
+
+  def update
+    @tag = Tag.find(params[:id])
+    if @tag.update(tag_params)
+      redirect_to admin_tags_path#, notice: '変更しました'
+    else
+      #flash[:danger] = @genre.errors.full_messages
+      redirect_to edit_admin_tag_path(@tag)
+    end
+  end
+
+  private
+
+  def tag_params
+    params.require(:tag).permit(:name)
   end
 end
--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,6 +1,7 @@
 class Public::PostsController < ApplicationController
-  #before_action :authenticate_user!
-  #before_action :ensure_correct_user, only: [:edit, :update, :destroy]
+  before_action :authenticate_user!
+  before_action :ensure_correct_user, only: [:edit, :update, :destroy]
+  skip_before_action :authenticate_user!, only: [:guest_sign_in]
 
   def new
     @post = Post.new
