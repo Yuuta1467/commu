ec56643525a266c769c0bf12297f7d3e6fbbbc0f
Yuuta1467 <s1341011we@gmail.com>
2023-05-13 08:01:11 +0000

    [admin会員投稿、コメント機能作成]

--- app/controllers/admin/post_comments_controller.rb
+++ app/controllers/admin/post_comments_controller.rb
@@ -7,4 +7,11 @@ class Admin::PostCommentsController < ApplicationController
 
   def edit
   end
+
+  def destroy
+    @post_comment = PostComment.find(params[:id])
+    @user = @post_comment.user
+    @post_comment.destroy
+    redirect_to admin_user_path(@user)
+  end
 end
--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -8,4 +8,11 @@ class Admin::PostsController < ApplicationController
 
   def edit
   end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @user = @post.user
+    @post.destroy
+    redirect_to admin_user_path(@user)
+  end
 end
