c89cb83221c9d41c3d6197a2d2b15535b4e5b7f1
Yuuta1467 <s1341011we@gmail.com>
2023-05-24 16:01:05 +0000

    [seedファイル記述]

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-Admin.create!(
-  email: 'admin@commu',
-  password: 'testtest'
-)
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -9,3 +5,49 @@ Admin.create!(
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Admin.create!(
+  email: 'admin@commu',
+  password: 'testtest'
+)
+
+users = User.create!(
+  [
+    {email: 'a@a', name: 'ゆーた', password: 'password', introduction: 'よろしく', helper_type: '未設定', is_deleted: 'false'},
+    {email: 'a@a1', name: 'ゆーた１', password: 'password', introduction: 'よろしく',  helper_type: '介護職', is_deleted: 'false'},
+    {email: 'a@a2', name: 'ゆーた２', password: 'password', introduction: 'よろしく', helper_type: '未設定', is_deleted: 'false'},
+    {email: 'a@a3', name: 'ゆーた３', password: 'password', introduction: 'よろしく', helper_type: '在宅介護', is_deleted: 'false'},
+    {email: 'a@a4', name: 'ゆーた４', password: 'password', introduction: 'よろしく', helper_type: 'その他', is_deleted: 'false'},
+    {email: 'a@a5', name: 'ゆーた５', password: 'password', introduction: 'よろしく', helper_type: '介護職', is_deleted: 'false'},
+    {email: 'a@a6', name: 'ゆーた６', password: 'password', introduction: 'よろしく', helper_type: 'その他', is_deleted: 'false'},
+    {email: 'a@a7', name: 'ゆーた７', password: 'password', introduction: 'よろしく', helper_type: '在宅介護', is_deleted: 'true'}
+  ]
+)
+
+tags = Tag.create!(
+  [
+    {name: '悩み'},
+    {name: '相談'},
+    {name: '疑問'},
+    {name: '雑談'},
+    {name: 'その他'}
+  ]
+  )
+
+Post.create!(
+  [
+    {user_id: users[0].id, content: '悩みがあります', tag_id: tags[0].id },
+    {user_id: users[0].id, content: '相談があります', tag_id: tags[1].id },
+    {user_id: users[0].id, content: '疑問があります', tag_id: tags[2].id },
+    {user_id: users[0].id, content: '雑談しましょう', tag_id: tags[3].id },
+    {user_id: users[0].id, content: 'こんにちは', tag_id: tags[4].id },
+    {user_id: users[0].id, content: '悩んでいます', tag_id: tags[0].id },
+    {user_id: users[0].id, content: '介護について相談があります', tag_id: tags[1].id },
+    {user_id: users[0].id, content: '色々なお話をしましょう！', tag_id: tags[3].id },
+    {user_id: users[0].id, content: '介護について疑問があります', tag_id: tags[2].id },
+    {user_id: users[0].id, content: 'よろしくお願いします', tag_id: tags[4].id },
+    {user_id: users[0].id, content: '最近疲れています', tag_id: tags[4].id },
+    {user_id: users[0].id, content: '介護は大変ですか？', tag_id: tags[2].id }
+  ]
+  )
+
